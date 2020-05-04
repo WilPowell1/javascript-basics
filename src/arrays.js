dc2cf304308b5e7c0a099902fc8f4a3359e9e531
WilPowell1 <powellwil88@gmail.com>
2020-05-04 21:33:30 +0100

    Need to finish arrays and objects

--- src/arrays.js
+++ src/arrays.js
@@ -1,77 +1,77 @@
 const getNthElement = (index, array) => {
-  // your code here
+    return array[index % array.length];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+    return array.join();
 };
 
 const csvStringToArray = string => {
-  // your code here
+    return string.split([","]);
 };
 
 const addToArray = (element, array) => {
-  // your code here
+    array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+    array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+    return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+    // your code here
 };
 
 const onlyEven = numbers => {
-  // your code here
+    // your code here
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+    // your code here
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+    // your code here
 };
 
 const removeSpaces = string => {
-  // your code here
+    // your code here
 };
 
 const sumNumbers = numbers => {
-  // your code here
+    // your code here
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+    // your code here
 };
 
 module.exports = {
-  getNthElement,
-  arrayToCSVString,
-  csvStringToArray,
-  addToArray,
-  addToArray2,
-  removeNthElement,
-  numbersToStrings,
-  uppercaseWordsInArray,
-  reverseWordsInArray,
-  onlyEven,
-  removeNthElement2,
-  elementsStartingWithAVowel,
-  removeSpaces,
-  sumNumbers,
-  sortByLastLetter
+    getNthElement,
+    arrayToCSVString,
+    csvStringToArray,
+    addToArray,
+    addToArray2,
+    removeNthElement,
+    numbersToStrings,
+    uppercaseWordsInArray,
+    reverseWordsInArray,
+    onlyEven,
+    removeNthElement2,
+    elementsStartingWithAVowel,
+    removeSpaces,
+    sumNumbers,
+    sortByLastLetter
 };
--- src/objects.js
+++ src/objects.js
@@ -1,52 +1,56 @@
 const createPerson = (name, age) => {
-  // your code here
+    return { name, age };
 };
 
 const getName = object => {
-  // your code here
+    return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+    return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+    return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+    if (person.age > 65) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const getAges = people => {
-  // your code here
+    return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+
 };
 
 const findHondas = cars => {
-  // your code here
+    // your code here
 };
 
 const averageAge = people => {
-  // your code here
+    // your code here
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+    // your code here
 };
 
 module.exports = {
-  createPerson,
-  getName,
-  getProperty,
-  hasProperty,
-  isOver65,
-  getAges,
-  findByName,
-  findHondas,
-  averageAge,
-  createTalkingPerson
+    createPerson,
+    getName,
+    getProperty,
+    hasProperty,
+    isOver65,
+    getAges,
+    findByName,
+    findHondas,
+    averageAge,
+    createTalkingPerson
 };
